aece37cf76be618b44d1d54d20ebb3fccf18e0bc
Steven R. Baker <steven@stevenrbaker.com>
2022-03-28 22:16:03 +0200

    Fix reference from the sample.

--- Raphael/Raphael.m
+++ Raphael/Raphael.m
@@ -6,7 +6,7 @@ main (int argc, const char **argv, char** env)
 {
   [NSApplication sharedApplication];
 
-  [NSBundle loadGSMarkupNamed: @"Raphael"  owner: calculator];
+  [NSBundle loadGSMarkupNamed: @"Raphael"  owner: [NSApp delegate]];
   [NSApp run];
 
   return 0;
